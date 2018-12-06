a7e94beb2c537413af9e8d761ab42a1e3fa0ec47
Luis Rodríguez <luis.miguel.rodr@gmail.com>
2018-12-06 01:53:56 -0600

    Firebase usando angularfire2
    https://github.com/angular/angularfire2/blob/master/docs/ionic/v3.md
    // Hay que considerar algunos cambios porque las instrucciones vienen para Ionic v3.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,27 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBa5sX7TqBn61bnSza1FCnlWwX3lMUZihw",
+  authDomain: "yaciment.firebaseapp.com",
+  databaseURL: "https://yaciment.firebaseio.com",
+  storageBucket: "yaciment.appspot.com",
+  messagingSenderId: '722141019472'
+};
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, AngularFireModule.initializeApp(firebaseConfig), AngularFireDatabaseModule, AngularFireAuthModule],
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireDatabase,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/logintest1/logintest1.page.ts
+++ src/app/logintest1/logintest1.page.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase  from 'firebase/app';
+
 @Component({
   selector: 'app-logintest1',
   templateUrl: './logintest1.page.html',
   styleUrls: ['./logintest1.page.scss'],
 })
 export class Logintest1Page implements OnInit {
+  items: Observable<any[]>;
+  constructor(afDB: AngularFireDatabase, private afAuth: AngularFireAuth) {
+  this.items = afDB.list('cuisines').valueChanges();
+  }
 
-  constructor() { }
+    signInWithFacebook() {
+    this.afAuth.auth
+      .signInWithPopup(new firebase.auth.FacebookAuthProvider())
+      .then(res => console.log(res));
+  }
 
+  signOut() {
+    this.afAuth.auth.signOut();
+  }
+    
   ngOnInit() {
   }
 
